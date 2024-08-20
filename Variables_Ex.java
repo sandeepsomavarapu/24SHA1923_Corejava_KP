65ef7846964d14a704d1c68c3b3644f02eec20ad
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-08-20 13:12:19 +0530

    Day-1 Reference

---
+++ Variables_Ex.java
@@ -0,0 +1,16 @@
+class Variables_Ex{
+	public static void main(String args[])
+	{
+		byte age=98;
+		int marks=98567;
+		long contact=9494920342l;
+		float salary=23.45443598723492f;
+		double amount=23.45443598723492;
+		char gender='M';
+		boolean result=true;
+		
+System.out.println(age+" "+marks+" "+contact+" "+salary+" "+amount+" "+gender+" "+result);
+		
+		
+	}
+}
---
+++ Welcome.java
@@ -0,0 +1,19 @@
+class Welcome{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to java");
+	}
+}
+
+//compailation: .java-->user understandable lang
+
+//JDK-->JRE-->JVM(execute)machine-->computer(machine)-->0's 1's binary /byte
+
+//telugu--> translator  --->hindi-->translation
+
+//		      compailation		byte	execution
+//.java(user)--->compiler  --->.class-->JVM(binary)-->output
+
+//javac filename.java--->classname.class-->java classname-->output
+
+//i , v
